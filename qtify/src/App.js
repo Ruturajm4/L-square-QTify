91d56c7186a3575ec55166b4c0b2b5fefdf6f488
unknown <ruturaj.m4@gmail.com>
2024-06-30 18:16:17 +0530

    commit

---
+++ qtify/src/App.js
@@ -0,0 +1,27 @@
+
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import StyledEngineProvider from "@mui/material/StyledEngineProvider";
+import './App.css';
+import TopAlbums from "./components/TopAlbums/TopAlbums";
+import NewAlbums from "./components/NewAlbums/NewAlbums";
+ import Hero from "./components/Hero/Hero";
+ import Songs from "./components/Songs/Songs";
+
+
+
+function App() {
+  return(
+  <StyledEngineProvider injectFirst>
+    <Navbar />
+    <Hero/>
+    <TopAlbums/>
+    <NewAlbums/>
+    <Songs/>
+  </StyledEngineProvider>
+      
+   
+  );
+}
+
+export default App;
---
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+import Logo from "../Logo/Logo";
+import Button from "../Button/Button";
+import Search from "../Search/Seacrh";
+
+function Navbar(){
+return(
+    <nav className={styles.navbar}>
+      <Logo/>  
+      <Search placeholder="Search a album of your choice"
+        />
+      <Button>Give Feedback</Button>
+    </nav>
+);
+}
+export default Navbar;
